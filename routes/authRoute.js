39d69ef75573b4f7af95d2f917b1058cef8e5536
Ignacio Fernandez <nachof5cjs@gmail.com>
2023-12-13 19:57:38 -0300

    agregando refresh Token

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,5 +5,5 @@ const router = express.Router()
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
-router.get("/all-users", getAllUsers)
+router.get("/all-users", getallUser);
 module.exports = router;
